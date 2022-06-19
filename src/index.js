ccf2a1e75027c03db863a7eda670922830f7055e
gloriaoluwabori <gloriaokiokio@gmail.com>
2022-06-19 23:12:08 +0100

    Gloria prime number implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 // complete the function
 function prime (num) {
   // code goes here
+if
+(num === 1){
+  console.log(`${num} is not a prime number. `)
+}else if(num > 1){
+  for ( let no= 2; no < num; no++){
+    if (num % no === 0){
+      console.log(`${num} is not a prime number.`);
+    }else{
+      console.log(`${num} is a prime number.`)
+    }
+
+  }
+}
 }
 
 function solution (arg) {
